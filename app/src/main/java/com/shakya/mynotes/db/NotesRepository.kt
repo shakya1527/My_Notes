c0b72aeaca64cf19ef211dbdef8ba48dcb870460
shakya1527 <sinhashakya15@gmail.com>
2024-07-17 18:51:01 +0530

    Some Changes

--- app/src/main/java/com/shakya/mynotes/db/NotesRepository.kt
+++ app/src/main/java/com/shakya/mynotes/db/NotesRepository.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import androidx.room.Room
 
 class NotesRepository(private val context: Context) {
-    private var database: NotesDatabase = Room
+    private val database: NotesDatabase = Room
         .databaseBuilder(context, NotesDatabase::class.java, "notes.db")
         .fallbackToDestructiveMigration()
         .build()
+    private val dao = database.notesDao()
 
-    suspend fun insert(note: Note) = database.notesDao().insert(note)
-    suspend fun delete(note: Note) = database.notesDao().delete(note)
-    fun getAll() = database.notesDao().getAll()
-    suspend fun deleteAll() = database.notesDao().deleteAll()
+    suspend fun insert(note: Note) = dao.insert(note)
+    suspend fun delete(note: Note) = dao.delete(note)
+    fun getAll() = dao.getAll()
+    suspend fun deleteAll() = dao.deleteAll()
 }
